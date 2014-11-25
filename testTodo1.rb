b81d311731f9f7709e8d3cb6f5a770e05e983c6f
jeannieteo <jeannieteo78@gmail.com>
2014-11-26 00:59:42 +0800

    add 1 more fail login case just to see
    Commit this from the git clone in my window downloaded github

--- testTodo1.rb
+++ testTodo1.rb
@@ -1,6 +1,6 @@
 require 'selenium-webdriver'
-#RSPEC testTodo.rb to run this
-describe 'Login' do
+
+describe 'Signup fail' do
    
  before(:each) do
  @driver = Selenium::WebDriver.for :firefox
@@ -9,9 +9,9 @@ describe 'Login' do
  after(:each) do
  @driver.quit
  end
- 
-#case login
- it 'failed to login because of email is not signed up' do
+   
+#case login fails
+ it 'failed to signup' do
  @driver.get 'https://todo-sample-app.herokuapp.com'
  @driver.find_element(id: 'user_email').send_keys('notauser@fgt')
  @driver.find_element(id: 'user_password').send_keys('password')
@@ -19,6 +19,15 @@ describe 'Login' do
  checkthis = @driver.find_element(class: 'alert')
  checkthis.text.should eql('Invalid email or password.')
  #THIS did not work@driver.find_element(class: 'alert').display? should be_true
- 
  end
+
+it 'failed to signup2' do
+ @driver.get 'https://todo-sample-app.herokuapp.com'
+ @driver.find_element(id: 'user_email').send_keys('jeannieteo78@gmail.com')
+ @driver.find_element(id: 'user_password').send_keys('')
+ @driver.find_element(id: 'new_user').submit
+ checkthis = @driver.find_element(class: 'alert')
+ checkthis.text.should eql('Invalid email or password.')
+ end
+
 end
