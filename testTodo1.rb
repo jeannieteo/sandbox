30231fd8bb094a1c0cd174bbc2c4ef9c1d4d0702
jeannieteo <jeannieteo78@gmail.com>
2014-11-29 15:25:51 +0800

    add error and fixed link
    add more error cases and link click is working, (silly me I should keep
    everything in 1 testcase)

--- testTodo1.rb
+++ testTodo1.rb
@@ -4,6 +4,7 @@ describe 'login fail' do
    
  before(:each) do
  @driver = Selenium::WebDriver.for :firefox
+ @base_url = "https://todo-sample-app.herokuapp.com"
  end
    
  after(:each) do
@@ -12,7 +13,7 @@ describe 'login fail' do
    
 #case login fails
  it '1. failed to login-user not signed in' do
- @driver.get 'https://todo-sample-app.herokuapp.com'
+ @driver.get @base_url
  @driver.find_element(id: 'user_email').send_keys('notauser@fgt')
  @driver.find_element(id: 'user_password').send_keys('password')
  @driver.find_element(id: 'new_user').submit
@@ -22,39 +23,62 @@ describe 'login fail' do
  end
 
  it '2. failed to login-no password' do
- @driver.get 'https://todo-sample-app.herokuapp.com'
+ @driver.get @base_url
  @driver.find_element(id: 'user_email').send_keys('jeannieteo78@gmail.com')
  @driver.find_element(id: 'user_password').send_keys('')
  @driver.find_element(id: 'new_user').submit
  checkthis = @driver.find_element(class: 'alert')
  checkthis.text.should eql('Invalid email or password.')
  end
+ 
+  it '3. failed to login-no user email' do
+   @driver.get @base_url
+   @driver.find_element(id: 'user_email').send_keys('')
+   @driver.find_element(id: 'user_password').send_keys('11111111')
+   @driver.find_element(id: 'new_user').submit
+   checkthis = @driver.find_element(class: 'alert')
+   checkthis.text.should eql('Invalid email or password.')
+   end
+   
+  it '4. failed to login-wrong password' do
+    @driver.get @base_url
+    @driver.find_element(id: 'user_email').send_keys('jeannieteo78@gmail.com')
+    @driver.find_element(id: 'user_password').send_keys('234545656467')
+    @driver.find_element(id: 'new_user').submit
+    checkthis = @driver.find_element(class: 'alert')
+    checkthis.text.should eql('Invalid email or password.')
+     end
 end 
 
 describe 'login pass' do
      
    before(:each) do
    @driver = Selenium::WebDriver.for :firefox
+   @base_url = "https://todo-sample-app.herokuapp.com"
    end
      
    after(:each) do
-   #@driver.quit
+   @driver.quit
    end
 
    it 'login successful' do 
-   @driver.get 'https://todo-sample-app.herokuapp.com'
+   @driver.get @base_url
    @driver.find_element(id: 'user_email').send_keys('jeannieteo78@gmail.com')
    @driver.find_element(id: 'user_password').send_keys('12345678')
    @driver.find_element(id: 'new_user').submit
    @driver.find_element(class: "notice").text.should eql("Signed in successfully.")
-   checkthis = @driver.find_element(class: "alert")
-   checkthis.find_element(tag_name: "div").text.should eql("Hi Friend (jeannieteo78@gmail.com),")
+   @driver.find_element(tag_name: "div").text.should eql("Hi Friend (jeannieteo78@gmail.com),")
+   
    end
    
   it 'click on logout' do 
-   #@driver.find_element(href: 'https://todo-sample-app.herokuapp.com/users/sign_out').click
-   @selenium.click("link=Logout")
-   checkthis = @driver.find_element(class: "alert")
-   checkthis.find_element(tag_name: "div").text.should eql("log out successful")
+  @driver.get 'https://todo-sample-app.herokuapp.com'
+  @driver.find_element(id: 'user_email').send_keys('jeannieteo@gmail.com')
+  @driver.find_element(id: 'user_password').send_keys('11111111')
+  @driver.find_element(id: 'new_user').submit
+  @driver.find_element(class: "notice").text.should eql("Signed in successfully.")
+  @driver.find_element(tag_name: "div").text.should eql("Hi Friend (jeannieteo@gmail.com),")
+  @driver.find_element(:link, "Logout").click
+  @driver.find_element(tag_name: "div").text.should eql("log out successful")
    end
 end
