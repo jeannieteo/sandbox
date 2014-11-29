a028977222199ed884e7fd51139fca374d7ac3ff
jeannieteo <jeannieteo78@gmail.com>
2014-11-30 02:53:22 +0800

    Edit task
    edit the first task and check if the edited task is correct. once edit
    the task get pulled to the last task. I wanted to loop through the <tr>
    but the task are changing locations.

---
+++ TestToDoEditTask.rb
@@ -0,0 +1,37 @@
+require 'selenium-webdriver'
+
+describe 'add new task' do
+   
+   before(:each) do
+   @driver = Selenium::WebDriver.for :firefox
+   @base_url = "https://todo-sample-app.herokuapp.com"
+   @driver.get @base_url
+   @driver.find_element(id: 'user_email').send_keys('jeannieteo@gmail.com')
+   @driver.find_element(id: 'user_password').send_keys('11111111')
+   @driver.find_element(id: 'new_user').submit
+   end
+     
+   after(:each) do
+   @driver.quit
+   end
+
+ it 'click on edit to edit task' do 
+   
+   taskcount = @driver.find_elements(xpath: "html/body/table[1]/tbody/tr").size
+   puts "there are " + taskcount.to_s + "tasks"
+   #first save the task description for checking later (first task)
+   stringtocompare = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[2]").text 
+   #click on edit of the first task
+   @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[4]/a").click
+   # check that textbox is showing the correct task
+   checkthis = @driver.find_element(id: "task_description")
+   checkthis.attribute("value").should eql(stringtocompare) 
+   #add  I added to the task description
+   checkthis.send_keys(" I added")
+   @driver.find_element(class: "actions").submit
+   # check that tasklists shows the correct edited task
+   editstring = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[" + taskcount.to_s + "]/td[2]").text
+   editstring.should eql(stringtocompare + " I added") 
+  end
+
+end
