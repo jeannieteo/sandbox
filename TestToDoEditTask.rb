bde873c236c298164a7f304df7a4c1eaf7e60bdc
jeannieteo <jeannieteo78@gmail.com>
2014-12-02 00:36:41 +0800

    3 changes - it should run okay now
    remove cannot find task description because the page had not loaded.
    added the wait.
    Finsihed the part where we check there are at least 1 task to edit.
    Change SHOULD to EXPECT syntax to remove the depreciation warning..

--- TestToDoEditTask.rb
+++ TestToDoEditTask.rb
@@ -1,9 +1,10 @@
 require 'selenium-webdriver'
 
-describe 'add new task' do
+describe 'edit first task' do
    
    before(:each) do
      @driver = Selenium::WebDriver.for :firefox
+     @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
      @base_url = "https://todo-sample-app.herokuapp.com"
      @driver.get @base_url
      @driver.find_element(id: 'user_email').send_keys('jeannieteo@gmail.com')
@@ -15,23 +16,33 @@ describe 'add new task' do
      @driver.quit
    end
 
- it 'click on edit to edit task' do 
+  it 'click on edit to edit task' do 
+    
+    taskcount = @driver.find_elements(xpath: "html/body/table[1]/tbody/tr").size
+    if taskcount == 0
+      puts "add a task please"
+    else
+      puts "there are " + taskcount.to_s + "tasks"
+      #first save the task description for checking later (first task)
+      stringtocompare = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[2]").text 
+      #click on edit of the first task
+      puts "click on first task's edit link"
+    
+      @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[4]/a").click 
+      #wait until the page load because the script failed here
+      @wait.until { expect(@driver.find_element(tag_name: "h1").text).to eq("Editing task") }
+      #check that textbox is showing the correct task
+      checkthis = @driver.find_element(id: "task_description")
+      expect(checkthis.attribute("value")).to eq(stringtocompare) 
+    
+      #add  "I added" to the task description
+      checkthis.send_keys(" I added")
+      @driver.find_element(class: "actions").submit
+      # check that tasklists shows the correct edited task
+      editstring = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[" + taskcount.to_s + "]/td[2]").text
+      expect(editstring).to eq(stringtocompare + " I added") 
+    end
+    
+ end
    
-   taskcount = @driver.find_elements(xpath: "html/body/table[1]/tbody/tr").size
-   puts "there are " + taskcount.to_s + "tasks"
-   #first save the task description for checking later (first task)
-   stringtocompare = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[2]").text 
-   #click on edit of the first task
-   @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[4]/a").click
-   # check that textbox is showing the correct task
-   checkthis = @driver.find_element(id: "task_description")
-   checkthis.attribute("value").should eql(stringtocompare) 
-   #add  I added to the task description
-   checkthis.send_keys(" I added")
-   @driver.find_element(class: "actions").submit
-   # check that tasklists shows the correct edited task
-   editstring = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[" + taskcount.to_s + "]/td[2]").text
-   editstring.should eql(stringtocompare + " I added") 
-  end
-
 end
