955629ee08d4e5bc808b8c662475718089d4eb73
jeannieteo <jeannieteo78@gmail.com>
2014-12-06 05:23:21 +0800

    edit task changes and sign up changes

--- spec/TestToDoEditTask_spec.rb
+++ spec/TestToDoEditTask_spec.rb
@@ -1,22 +1,18 @@
-require 'selenium-webdriver'
-require "rspec/expectations"
+#filename : TestToDoEditTask_spec.rb
+
+require "spec_helper"
+require_relative "../Login"
+EDIT_LINK = {xpath: "html/body/table[1]/tbody/tr[1]/td[4]/a"}
+TASK_STRING = {xpath: "html/body/table[1]/tbody/tr[1]/td[2]"}
 
 describe 'edit first task' do
-   
+
+
    before(:each) do
-     @driver = Selenium::WebDriver.for :firefox
-     @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-     @base_url = "https://todo-sample-app.herokuapp.com"
-     @driver.get @base_url
-     @driver.find_element(id: 'user_email').send_keys('jeannieteo@gmail.com')
-     @driver.find_element(id: 'user_password').send_keys('11111111')
-     @driver.find_element(id: 'new_user').submit
+     @Login = Login.new(@driver)
+     @Login.with('jeannieteo78@gmail.com', '12345678')
    end
      
-   after(:each) do
-     @driver.quit
-   end
-
   it 'click on edit to edit task' do 
     
     taskcount = @driver.find_elements(xpath: "html/body/table[1]/tbody/tr").size
@@ -25,11 +21,11 @@ describe 'edit first task' do
     else
       puts "there are " + taskcount.to_s + "tasks"
       #first save the task description for checking later (first task)
-      stringtocompare = @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[2]").text 
+      stringtocompare = @driver.find_element(TASK_STRING).text 
       #click on edit of the first task
       puts "click on first task's edit link"
     
-      @driver.find_element(xpath: "html/body/table[1]/tbody/tr[1]/td[4]/a").click 
+      @driver.find_element(EDIT_LINK).click 
       #wait until the page load because the script failed here
       @wait.until { expect(@driver.find_element(tag_name: "h1").text).to eq("Editing task") }
       #check that textbox is showing the correct task
--- spec/TestToDoSignUp_spec.rb
+++ spec/TestToDoSignUp_spec.rb
@@ -9,14 +9,14 @@ describe 'SignUp' do
  end
   
  it '1. signup ' do
- @Signup.with('new@user', '11111111','11111111' )
+ @Signup.with('new@user.com', '11111111','11111111' )
  expect(@driver.find_element(class: "notice").text).to eq("Welcome! You have signed up successfully.")
  checkthis = @driver.find_element(class: "alert")
  expect(checkthis.find_element(tag_name: "div").text).to eq("Hi Friend (new@user),")
  end
 
  it '2. signup ' do
- @Signup.with('new1@user', '11111111','11111111' )
+ @Signup.with('new1@user.com', '11111111','11111111' )
   
   expect(@driver.find_element(class: "notice").text).to eq("Welcome! You have signed up successfully.")
   checkthis = @driver.find_element(class: "alert")
